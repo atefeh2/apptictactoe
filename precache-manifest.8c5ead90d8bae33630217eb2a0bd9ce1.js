667f4c694e03549e603988fe1665fdd71affc57b
atefeh2 <44230313+atefeh2@users.noreply.github.com>
2020-05-30 22:45:46 +0430

    Add files via upload

---
+++ precache-manifest.8c5ead90d8bae33630217eb2a0bd9ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf152f641ed5ca8bc3d8243a4cd1abda",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "c6e513f6c9e77e17a1d9",
+    "url": "/tictactoe/static/css/main.a8b4ea5a.chunk.css"
+  },
+  {
+    "revision": "ab3786026fcb6dc17279",
+    "url": "/tictactoe/static/js/2.266c45ce.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tictactoe/static/js/2.266c45ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e513f6c9e77e17a1d9",
+    "url": "/tictactoe/static/js/main.b874b00b.chunk.js"
+  },
+  {
+    "revision": "d6a76b3547955ded53da",
+    "url": "/tictactoe/static/js/runtime-main.986fe205.js"
+  }
+]);
